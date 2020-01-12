7259d8004817bc540377b7620e3bdcbb41f06574
Christopher Zenzel <christopher@venerate.tech>
2020-01-12 04:13:07 -0500

    Enable dos2unix fixes, perform build inside docker container since we are more restricted on Windows

--- buildroot-v86/build-v86.sh
+++ buildroot-v86/build-v86.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -e
 
-# Perform dos2unix
-cd /buildroot-v86
-find . -type f -print0 | xargs -0 dos2unix
-
 # CD to the right directory
-cd ~/buildroot-2018.02
+cd /root/buildroot-2018.02
 
 # Build our v86 defconfig along with license files.
 echo $PWD
